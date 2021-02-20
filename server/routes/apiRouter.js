3873ce92a3a086d7705a2ef3c7f1b5504a52b239
Karavella03 <nikitke06060@gmail.com>
2021-02-21 02:14:27 +0500

    minor fixes

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -8,8 +8,11 @@ const authorization = require('../middleWares/authorization')
 const jsonParser = express.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.use('/api', urlencodedParser, registerRouter)
-router.use('/api', urlencodedParser, loginRouter)
+router.use('/api', urlencodedParser)
+router.use('/api', jsonParser)
+
+router.use('/api', registerRouter)
+router.use('/api', loginRouter)
 router.get('/api/test', authorization, (req, res) => {
     res.status(200).json(req.user)
 })
