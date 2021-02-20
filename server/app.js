1c4eec0513ca03b5597f8a34904b6ab4d7e9cadc
Karavella03 <nikitke06060@gmail.com>
2021-02-21 01:09:20 +0500

    adding registration

--- server/app.js
+++ server/app.js
@@ -11,18 +11,20 @@ mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: t
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err))
 
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const jsonParser = express.json()
 
 app.use(require('cors')())
 app.use(express.static('src'))
-app.use(jsonParser, registerRouter)
+
+app.use('/api', urlencodedParser, registerRouter)
 
 app.use((err, req, res, next) => {
     errorHandler(err, res, 500)
 })
 
-app.use((req, res) => {
+app.use(urlencodedParser, (req, res) => {
+    console.log(req.body)
     errorHandler(new Error('404 Page not found!'), res, 404)
 })
 
--- server/routes/registerRouter.js
+++ server/routes/registerRouter.js
@@ -1,13 +1,33 @@
 const experess = require('express')
+const bcrypt = require('bcryptjs')
 const router = experess.Router()
 const User = require('../models/User')
-const Group = require('../models/Group')
-const Record = require('../models/Record')
 const errorHandler = require('../utils/errorHandler')
 
-router.post('/api/register', async (req, res) => {
-
+router.post('/register', async (req, res) => {
+    console.log(req.body)
+    const login = req.body.login.toLowerCase()
+    const conditate = await User.findOne({ login })
+    if (conditate) {
+        errorHandler(new Error('Пользователь уже существует'), res, 409)
+    } else {
+        const salt = await bcrypt.genSalt(10)
+        const password = req.body.password
+        const passwordHash = await bcrypt.hash(password, salt)
+        const user = new User({
+            login,
+            passwordHash,
+            name: req.body.name,
+            surname: req.body.surname,
+            description: req.body.description
+        })
+        try {
+            await user.save()
+            res.status(200).json(user)
+        } catch (err) {
+            errorHandler(err, res)
+        }
+    }
 })
-
 
 module.exports = router
